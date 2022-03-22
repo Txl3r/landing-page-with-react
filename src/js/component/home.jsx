3df9906e3060fe1d1bf5a0318aa029a7fff1b454
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-03-22 19:14:56 +0000

    made changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,81 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Features
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Pricing
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link disabled" href="#">
+								Disabled
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div className="jumbotron">
+				<h1 className="display-4">Hello, world!</h1>
+				<p className="lead">
+					This is a simple hero unit, a simple jumbotron-style
+					component for calling extra attention to featured content or
+					information.
+				</p>
+				<p>
+					It uses utility classes for typography and spacing to space
+					content out within the larger container.
+				</p>
+				<p className="lead">
+					<a
+						className="btn btn-primary btn-lg"
+						href="#"
+						role="button">
+						Learn more
+					</a>
+				</p>
+			</div>
+			<div className="card" style={{ width: "18rem;" }}>
+				<img className="card-img-top" src="..." alt="Card image cap" />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
